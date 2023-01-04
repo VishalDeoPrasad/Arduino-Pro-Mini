3435e1821c37c3a22c5eda3942aa458613f2aa12
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-01-05 01:54:36 +0530

    blinkled

--- BlinkLED.ino
+++ BlinkLED.ino
@@ -1,11 +1,12 @@
+int ledPin = A2;
 void setup() {
-  pinMode(7, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(7, HIGH); // LED On
+  digitalWrite(ledPin, HIGH); // LED On
   delay(500);             // delay (1000 = 1s)
-  digitalWrite(7, LOW);  // LED Off
+  digitalWrite(ledPin, LOW);  // LED Off
   delay(500);             // delay
 
 }
