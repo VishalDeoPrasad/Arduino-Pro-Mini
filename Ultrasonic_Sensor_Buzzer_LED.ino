51095b7aa0e6e5c03853565a2a3ac8df34db68e0
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-01-26 00:32:14 +0530

    Ultrasonic sensor buzzer

---
+++ Ultrasonic_Sensor_Buzzer_LED.ino
@@ -0,0 +1,29 @@
+int trigPin = 6;
+int echoPin = 4;
+int ledPin = 7;
+int buzzerPin = 12;
+void setup() {
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  digitalWrite(trigPin, LOW); // clean the trigPin
+  Serial.begin(9600); 
+}
+void loop() {
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); // triger for 10 micro seconds
+  digitalWrite(trigPin, LOW);
+  long duration = pulseIn(echoPin, HIGH); // Read returns the sound wave travel time in microseconds
+  int distance = duration * 0.034 / 2;   // Calculating the distance
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  if (distance < 20){
+    digitalWrite(buzzerPin, HIGH);
+    digitalWrite(ledPin, HIGH);    
+  }
+  else{
+    digitalWrite(buzzerPin, LOW);
+    digitalWrite(ledPin, LOW);
+  }
+}
